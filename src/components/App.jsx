94e8c76ec9f8e5d0000af355550c5aeec40882cf
Vallyblink <alabama.toj@gmail.com>
2023-06-10 16:38:29 +0300

    lastchange

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import SearchBar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Modal from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    imageName: '',
+    showModal: false,
+    selectedImageUrl: ''
+  };
+
+  handleImageSubmit = (imageName) => {
+    this.setState({ imageName: imageName });
+  };
+
+  toggleModal = (imageUrl) => {
+    this.setState((prevState) => ({
+      showModal: !prevState.showModal,
+      selectedImageUrl: imageUrl
+    }));
+  };
+
+  render() {
+    const { imageName, showModal, selectedImageUrl } = this.state;
+
+    return (
+      <div>
+        <ToastContainer />
+        <SearchBar onSearch={this.handleImageSubmit} />
+        <ImageGallery imageName={imageName} onSelectImage={this.toggleModal} />
+        {showModal && (
+          <Modal isOpen={showModal} imageUrl={selectedImageUrl} onClose={this.toggleModal} />
+        )}
+      </div>
+    );
+  }
+}
